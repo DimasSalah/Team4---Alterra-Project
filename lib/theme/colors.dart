f73cf3479baf788705ff3096d7bf5687394d7f0b
Dimas Salahuddin <dimassalah10@gmail.com>
2024-03-25 22:13:59 +0700

    style: add color and font style for design system

---
+++ lib/theme/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+//primary colors
+Color primary = const Color(0xFFDB3022);
+
+//neutral colors
+Color backgroundColor = const Color(0xFFF9F9F9);
+Color white = const Color(0xFFFFFFFF);
+Color grey = const Color(0xFF9B9B9B);
+Color black = const Color(0xFF222222);
---
+++ lib/theme/text_styles.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+TextStyle regular = const TextStyle(
+  fontFamily: "Metropolis",
+  fontWeight: FontWeight.w400,
+);
+
+TextStyle medium = const TextStyle(
+  fontFamily: "Metropolis",
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle semiBold = const TextStyle(
+  fontFamily: "Metropolis",
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle bold = const TextStyle(
+  fontFamily: "Metropolis",
+  fontWeight: FontWeight.w700,
+);
